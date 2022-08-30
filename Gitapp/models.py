b3c0457e91ef886cea1a69423189fa1d60adf8c7
Pooja Bhagwat <bhagwatp15@gmail.com>
2022-08-30 22:57:34 +0530

    Hospital Model Added

--- Gitapp/models.py
+++ Gitapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Hospital(models.Model):
+    name =  models.CharField(max_length=200)
+    contact_no = models.IntegerField()
+    address = models.CharField(max_length=200)
+
+
+    class Meta:
+        db_table = "Hospital"
+
