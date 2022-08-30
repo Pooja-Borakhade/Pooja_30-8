70b425b398e3b44ec35cafa3ed4a9a699a2e5082
Lalit Chaudhari <lalit.chaudhari0@gmail.com>
2022-08-30 22:56:59 +0530

    Doctor Model added

--- Gitapp/models.py
+++ Gitapp/models.py
@@ -1,3 +1,12 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+    name = models.CharField(max_length=100)
+    quali = models.CharField(max_length=100)
+    number = models.IntegerField()
+    
+    class Meta:
+        db_name = 'Doctor'
+    
